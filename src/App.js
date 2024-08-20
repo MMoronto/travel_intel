89c77206260d8fb249760703c5a9c7f485b96bd3
Mmoronto <t2wycemo@gmail.com>
2024-08-20 11:57:53 -0500

    create component folders and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
     return (
-        <div>
-        <h1>My React App!</h1>
-        </div>
+        <>
+        <CssBaseline />    
+        <Header />
+
+        </>
     );
 }
 
